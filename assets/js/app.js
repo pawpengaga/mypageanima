7b34058e2cf74ce3eb441cd1f220287693f11081
Erick Rivera <yomellamoerick.771@gmail.com>
2023-06-27 03:57:58 -0400

    first version of the site hehe

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,9 @@
-const background = document.querySelector('.animaheader');
-const char = document.querySelector('.animachar');
+const background = document.querySelector('#animaheader');
+const char1 = document.querySelector('#animachar1');
+const char2 = document.querySelector('#animachar2');
+
+const ui1 = document.querySelector('.arrow');
+const ui2 = document.querySelector('.arrow2');
 
 lottie.loadAnimation({
     container: background,
@@ -7,14 +11,41 @@ lottie.loadAnimation({
     bounce: true,
     loop: false,
     autoplay: true,
-    path :'../bg/background.json'
+    path :'../assets/anima/bg/background.json'
 });
 
 lottie.loadAnimation({
-    container: char,
+    container: char1,
     renderer: 'svg',
     bounce: true,
     loop: true,
     autoplay: true,
-    path :'../char/promo.json'
+    path :'../assets/anima/char1/promo.json'
+});
+
+lottie.loadAnimation({
+    container: char2,
+    renderer: 'svg',
+    bounce: true,
+    loop: true,
+    autoplay: true,
+    path :'../assets/anima/char2/mom.json'
+});
+
+lottie.loadAnimation({
+    container: ui1,
+    renderer: 'svg',
+    bounce: true,
+    loop: true,
+    autoplay: true,
+    path :'../assets/anima/ui/myarrow.json'
+});
+
+lottie.loadAnimation({
+    container: ui2,
+    renderer: 'svg',
+    bounce: true,
+    loop: true,
+    autoplay: true,
+    path :'../assets/anima/ui/myarrow.json'
 });
